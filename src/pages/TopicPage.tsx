5c41dc2dc03e33ea2d09f0d55149edf3eb57b431
Chun-gu <chungu.yi92@gmail.com>
2023-05-24 10:00:06 +0900

    주제상세페이지 생성

---
+++ src/pages/TopicPage.tsx
@@ -0,0 +1,21 @@
+export default function TopicPage() {
+  return (
+    <>
+      <section>
+        <div>
+          <span>작성일자</span>
+          <button>공유</button>
+        </div>
+        <h1>Q. 주제 제목</h1>
+        <p>내용: ~~~~~~~~</p>
+        <div>
+          <button>선택지A</button>
+          <button>선택지B</button>
+        </div>
+      </section>
+      <section>
+        <ul></ul>
+      </section>
+    </>
+  )
+}
