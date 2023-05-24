8ebe766847d060eeeeaaa90afa59d843dcd13a33
Chun-gu <chungu.yi92@gmail.com>
2023-05-24 11:17:31 +0900

    Topic 타입에 title 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,14 @@
 export type Topic = {
   id: string
+  title: string
   content: string
   firstOption: { content: string; count: number }
   secondOption: { content: string; count: number }
   createdAt: string
 }
 
-export type NewTopic = Pick<
-  Topic,
-  'content' | 'firstOption' | 'secondOption'
-> & { password: string }
+export type NewTopic = Pick<Topic, 'title' | 'content'> & {
+  firstOption: { content: string }
+  secondOption: { content: string }
+  password: string
+}
