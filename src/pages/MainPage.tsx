c4241570827942a17e827076d9b0a897f380b41f
Chun-gu <chungu.yi92@gmail.com>
2023-05-24 09:59:40 +0900

    App페이지=>Main페이지 변경

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+export default function MainPage() {
+  return (
+    <>
+      <section>
+        <h2>불타는 주제</h2>
+        <ul></ul>
+      </section>
+      <section>
+        <h2>새로운 주제</h2>
+        <ul></ul>
+      </section>
+      <button>글쓰기</button>
+    </>
+  )
+}
