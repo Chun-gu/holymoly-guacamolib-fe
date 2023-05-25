cd31643917abe36986d7387a86086c3bca79a3dc
Chun-gu <chungu.yi92@gmail.com>
2023-05-25 09:44:24 +0900

    주제 상세 페이지에서 주제의 데이터 렌더링

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -1,19 +1,38 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+
+import { getTopic } from '@/api/topic'
+
 export default function TopicPage() {
+  const { topicId } = useParams() as { topicId: string }
+  const {
+    isLoading,
+    data: topic,
+    isError,
+  } = useQuery({
+    queryKey: ['topics', topicId],
+    queryFn: () => getTopic(topicId),
+  })
+
+  if (isLoading) return <div>로딩 중..</div>
+  if (isError) return <div>에러!</div>
+
   return (
     <>
       <section>
         <div>
-          <span>작성일자</span>
+          <span>{topic.createdAt}</span>
           <button>공유</button>
         </div>
-        <h1>Q. 주제 제목</h1>
-        <p>내용: ~~~~~~~~</p>
+        <h1>Q. {topic.title}</h1>
+        <p>내용: {topic.content}</p>
         <div>
-          <button>선택지A</button>
-          <button>선택지B</button>
+          <button>{topic.firstOption.content}</button>
+          <button>{topic.secondOption.content}</button>
         </div>
       </section>
       <section>
+        <h3>댓글 목록</h3>
         <ul></ul>
       </section>
     </>
