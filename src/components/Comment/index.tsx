01c3eaf25a0172a537149a2e42172ee0154d6a13
Chun-gu <chungu.yi92@gmail.com>
2023-05-27 04:30:20 +0900

    내가 작성한 댓글일 경우만 삭제 버튼 렌더링하도록 변경

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -4,6 +4,7 @@ import { useMutation } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 
 import { commentKey, deleteComment } from '@/api/comment'
+import { useLocalStorage } from '@/hooks'
 import { formatDate } from '@/lib'
 import { queryClient } from '@/main'
 
@@ -20,6 +21,8 @@ type Props = {
 export default function Comment({ comment }: Props) {
   const { topicId } = useParams() as { topicId: string }
   const passwordInputRef = useRef<HTMLInputElement>(null)
+  const [createdComments] = useLocalStorage<string[]>('createdComments', [])
+  const isMyComment = createdComments.includes(comment.id)
 
   const mutation = useMutation({
     mutationFn: deleteComment,
@@ -42,12 +45,14 @@ export default function Comment({ comment }: Props) {
         <span>{formatDate(comment.createdAt, 'absolute')}</span>
       </div>
       <p>{comment.content}</p>
-      <form onSubmit={handleDeleteComment}>
-        <label>
-          비밀번호 <input type="password" ref={passwordInputRef} />
-        </label>
-        <button>삭제</button>
-      </form>
+      {isMyComment && (
+        <form onSubmit={handleDeleteComment}>
+          <label>
+            비밀번호 <input type="password" ref={passwordInputRef} />
+          </label>
+          <button>삭제</button>
+        </form>
+      )}
     </>
   )
 }
