e50cdd946232d9cc1a14999ca7d91b757f942408
Chun-gu <chungu.yi92@gmail.com>
2023-05-27 04:11:43 +0900

    내가 만든 주제일 경우만 삭제 버튼 렌더하도록 변경

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -5,12 +5,15 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 import { deleteTopic, getTopic, topicKeys } from '@/api/topic'
 import { CommentInput, CommentList } from '@/components'
+import { useLocalStorage } from '@/hooks'
 import { formatDate } from '@/lib'
 
 export default function TopicPage() {
   const { topicId } = useParams() as { topicId: string }
   const passwordInputRef = useRef<HTMLInputElement>(null)
   const navigate = useNavigate()
+  const [createdTopics] = useLocalStorage<string[]>('createdTopics', [])
+  const isMyTopic = createdTopics.includes(topicId)
 
   const mutation = useMutation({
     mutationFn: deleteTopic,
@@ -43,13 +46,15 @@ export default function TopicPage() {
       <section>
         <div>
           <span>{formatDate(topic.createdAt, 'relative')}</span>
-          <form onSubmit={handleDeleteTopic}>
-            <label>
-              비밀번호
-              <input type="password" ref={passwordInputRef} />
-            </label>
-            <button>삭제</button>
-          </form>
+          {isMyTopic && (
+            <form onSubmit={handleDeleteTopic}>
+              <label>
+                비밀번호
+                <input type="password" ref={passwordInputRef} />
+              </label>
+              <button>삭제</button>
+            </form>
+          )}
           <button>공유</button>
         </div>
         <h1>Q. {topic.title}</h1>
