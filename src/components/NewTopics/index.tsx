4d215583e2fcb4a97b2948611d125dc59156ae3a
Chun-gu <chungu.yi92@gmail.com>
2023-05-27 07:51:15 +0900

    새로운 주제 목록에 무한 불러오기 적용

--- src/components/NewTopics/index.tsx
+++ src/components/NewTopics/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 
 import { getNewTopics, topicKeys } from '@/api/topic'
@@ -6,28 +6,47 @@ import { NewTopic } from '@/components'
 
 export default function NewTopics() {
   const {
-    data: newTopics,
     isLoading,
     isError,
-  } = useQuery({
+    data: newTopics,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: topicKeys.new,
     queryFn: getNewTopics,
+    getNextPageParam: ({ nextPage }) => nextPage,
   })
 
   if (isLoading) return <div>로딩 중...</div>
   if (isError) return <div>에러!</div>
 
-  if (newTopics.length === 0) return <div>주제가 하나도 없어요.</div>
+  if (newTopics.pages.length === 0) return <div>주제가 하나도 없어요.</div>
 
   return (
     <ul>
-      {newTopics.map((newTopic) => (
-        <li key={newTopic.id}>
-          <Link to={`/topics/${newTopic.id}`}>
-            <NewTopic topicId={newTopic.id} />
-          </Link>
-        </li>
-      ))}
+      {newTopics.pages.map(({ topics }) =>
+        topics.map((topic) => (
+          <li key={topic.id}>
+            <Link to={`/topics/${topic.id}`}>
+              <NewTopic topicId={topic.id} />
+            </Link>
+          </li>
+        )),
+      )}
+      <div>
+        <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}>
+          {isFetchingNextPage
+            ? '더 불러오는 중...'
+            : hasNextPage
+            ? '더 불러오기'
+            : '더 불러올 게 없어요'}
+        </button>
+      </div>
+      <div>{isFetching && !isFetchingNextPage ? '가져오는 중' : null}</div>
     </ul>
   )
 }
