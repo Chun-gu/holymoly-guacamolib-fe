681f0173514f29437d84bfeae95796aa8d055581
Chun-gu <chungu.yi92@gmail.com>
2023-05-27 06:22:19 +0900

    주제 상세 페이지에서 주제 컴포넌트 분리

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -1,103 +1,10 @@
-import { FormEvent, useRef } from 'react'
-
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { useNavigate, useParams } from 'react-router-dom'
-
-import { deleteTopic, getTopic, topicKeys, vote } from '@/api/topic'
-import { CommentInput, CommentList } from '@/components'
-import { useLocalStorage } from '@/hooks'
-import { formatDate } from '@/lib'
-import { queryClient } from '@/main'
+import { CommentInput, CommentList, Topic } from '@/components'
 
 export default function TopicPage() {
-  const { topicId } = useParams() as { topicId: string }
-  const passwordInputRef = useRef<HTMLInputElement>(null)
-  const navigate = useNavigate()
-  const [createdTopics, setCreatedTopics] = useLocalStorage<string[]>(
-    'createdTopics',
-    [],
-  )
-  const isMyTopic = createdTopics.includes(topicId)
-  const [votedTopics, setVotedTopics] = useLocalStorage<string[]>(
-    'votedTopics',
-    [],
-  )
-  const isVotedTopic = votedTopics.includes(topicId)
-
-  const deleteMutation = useMutation({
-    mutationFn: deleteTopic,
-    onSuccess: ({ deletedTopicId }) => {
-      const newCreatedTopics = createdTopics.filter(
-        (topicId) => topicId !== deletedTopicId,
-      )
-      setCreatedTopics(newCreatedTopics)
-      navigate('/')
-    },
-  })
-
-  const voteMutation = useMutation({
-    mutationFn: vote,
-    onSuccess: ({ votedTopicId }) => {
-      setVotedTopics([...votedTopics, votedTopicId])
-      queryClient.invalidateQueries(topicKeys.topic(topicId))
-    },
-  })
-
-  function handleVote(votedOption: string) {
-    voteMutation.mutate({ topicId, votedOption })
-  }
-
-  function handleDeleteTopic(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const password = passwordInputRef.current?.value
-
-    if (!password) return alert('비밀번호를 입력하세요')
-
-    deleteMutation.mutate({ topicId, password })
-  }
-
-  const {
-    isLoading,
-    data: topic,
-    isError,
-  } = useQuery({
-    queryKey: topicKeys.topic(topicId),
-    queryFn: () => getTopic(topicId),
-  })
-
-  if (isLoading) return <div>로딩 중..</div>
-  if (isError) return <div>에러!</div>
-
   return (
     <>
       <section>
-        <div>
-          <span>{formatDate(topic.createdAt, 'relative')}</span>
-          {isMyTopic && (
-            <form onSubmit={handleDeleteTopic}>
-              <label>
-                비밀번호
-                <input type="password" ref={passwordInputRef} />
-              </label>
-              <button>삭제</button>
-            </form>
-          )}
-          <button>공유</button>
-        </div>
-        <h1>Q. {topic.title}</h1>
-        <p>내용: {topic.content}</p>
-        <div>
-          <button
-            onClick={() => handleVote('firstOption')}
-            disabled={isVotedTopic}>
-            {topic.firstOption.content}/{topic.firstOption.count}
-          </button>
-          <button
-            onClick={() => handleVote('secondOption')}
-            disabled={isVotedTopic}>
-            {topic.secondOption.content}/{topic.secondOption.count}
-          </button>
-        </div>
+        <Topic />
       </section>
       <section>
         <h3>댓글</h3>
