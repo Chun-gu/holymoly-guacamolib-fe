5409f6d1a94188685facbcb00adc02480c117346
Chun-gu <chungu.yi92@gmail.com>
2023-05-28 05:45:04 +0900

    불타는 주제 단일 아이템 스타일링

--- src/components/HotTopic/index.tsx
+++ src/components/HotTopic/index.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { getTopic, topicKeys, vote } from '@/api/topic'
 import { useLocalStorage } from '@/hooks'
@@ -41,22 +42,69 @@ export default function HotTopic({ topicId }: Props) {
   if (isError) return <div>에러!</div>
 
   return (
-    <div>
-      <h3>Q. {topic.title}</h3>
-      <p>{topic.content}</p>
+    <Container>
       <div>
-        <button
+        <Title>Q. {topic.title}</Title>
+        <Content>{topic.content}</Content>
+      </div>
+      <OptionContainer>
+        <Option
           onClick={() => handleVote('firstOption')}
           disabled={isVotedTopic}>
-          {topic.firstOption.content}/{topic.firstOption.count}
-        </button>
-        <button
+          <span>{topic.firstOption.content}</span>
+        </Option>
+        <Option
           onClick={() => handleVote('secondOption')}
           disabled={isVotedTopic}>
-          {topic.secondOption.content}/{topic.secondOption.count}
-        </button>
-      </div>
+          {topic.secondOption.content}
+        </Option>
+      </OptionContainer>
       {isVotedTopic && <Link to={`/topics/${topicId}`}>의견 보러 가기</Link>}
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 301px;
+  height: 221px;
+  background-color: #ffffff;
+  padding: 18px 17px;
+  border: 1px solid #33ac5f;
+  border-radius: 20px;
+`
+
+const Title = styled.h3`
+  font-size: 14px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  margin-bottom: 12px;
+`
+
+const Content = styled.p`
+  font-size: 12px;
+  line-height: 1.8;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`
+
+const OptionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Option = styled.button`
+  width: 127px;
+  height: 84px;
+  color: #ffffff;
+  font-size: 14px;
+  line-height: 1.5;
+  background-color: #38af61;
+  border-radius: 16px;
+  cursor: pointer;
+`
