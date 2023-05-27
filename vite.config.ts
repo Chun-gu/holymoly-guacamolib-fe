e29aace553ec06af0f2903c025ad9a318342794d
Chun-gu <chungu.yi92@gmail.com>
2023-05-27 18:38:42 +0900

    svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import tsconfigPath from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPath()],
+  plugins: [react(), tsconfigPath(), svgr()],
+  // server: {
+  //   proxy: {
+  //     '^/api': {
+  //       target: 'http://101.101.208.191',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/\/api/, ''),
+  //       secure: false,
+  //     },
+  //   },
+  // },
 })
