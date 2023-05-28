4b9067c420f3749d05f1ea86e7c868c2c4a79aee
Chun-gu <chungu.yi92@gmail.com>
2023-05-28 10:03:14 +0900

    단일 댓글 스타일링

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -2,8 +2,10 @@ import { FormEvent, useRef } from 'react'
 
 import { useMutation } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { commentKey, deleteComment } from '@/api/comment'
+import { ReactComponent as User } from '@/assets/user-icon.svg'
 import { useLocalStorage } from '@/hooks'
 import { formatDate } from '@/lib'
 import { queryClient } from '@/main'
@@ -49,19 +51,53 @@ export default function Comment({ comment }: Props) {
 
   return (
     <>
-      <div>
-        <span>익명{comment.index}</span>
-        <span>{formatDate(comment.createdAt, 'absolute')}</span>
-      </div>
-      <p>{comment.content}</p>
-      {isMyComment && (
-        <form onSubmit={handleDeleteComment}>
-          <label>
-            비밀번호 <input type="password" ref={passwordInputRef} />
-          </label>
-          <button>삭제</button>
-        </form>
-      )}
+      <CommentHeader>
+        <UserName>
+          <User />
+          익명 {comment.index}
+        </UserName>
+        <DateWrapper>
+          <span>{formatDate(comment.createdAt, 'absolute')}</span>
+          {isMyComment && <button>삭제</button>}
+        </DateWrapper>
+      </CommentHeader>
+      <Content>{comment.content}</Content>
+      {/* <form onSubmit={handleDeleteComment}>
+        <label>
+          비밀번호 <input type="password" ref={passwordInputRef} />
+        </label>
+      </form> */}
     </>
   )
 }
+
+const CommentHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #38af61;
+  margin-bottom: 4px;
+`
+const UserName = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  svg {
+    margin-right: 8px;
+  }
+`
+const DateWrapper = styled.div`
+  font-size: 12px;
+  span {
+    margin-right: 10px;
+  }
+  button {
+    color: #38af61;
+  }
+`
+const Content = styled.p`
+  font-size: 12px;
+  color: #7f7f7f;
+  line-height: 1.5;
+  word-wrap: break-word;
+`
