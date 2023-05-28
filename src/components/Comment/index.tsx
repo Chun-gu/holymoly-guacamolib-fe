d8639f2588624e443ad51b40d9892d9dbfc2e252
Chun-gu <chungu.yi92@gmail.com>
2023-05-28 13:44:03 +0900

    댓글 삭제 모달 구현

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,6 +1,7 @@
-import { FormEvent, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useMutation } from '@tanstack/react-query'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -21,15 +22,15 @@ type Props = {
 }
 
 export default function Comment({ comment }: Props) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
   const { topicId } = useParams() as { topicId: string }
-  const passwordInputRef = useRef<HTMLInputElement>(null)
   const [createdComments, setCreatedComments] = useLocalStorage<string[]>(
     'createdComments',
     [],
   )
   const isMyComment = createdComments.includes(comment.id)
 
-  const mutation = useMutation({
+  const deleteMutation = useMutation({
     mutationFn: deleteComment,
     onSuccess: ({ deletedCommentId }) => {
       const newCreatedComments = createdComments.filter(
@@ -40,14 +41,32 @@ export default function Comment({ comment }: Props) {
     },
   })
 
-  function handleDeleteComment(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const commentId = comment.id
-    const password = passwordInputRef.current?.value
-
-    if (!password) return alert('비밀번호를 입력해주세요.')
-    mutation.mutate({ topicId, commentId, password })
+  function toggleDeleteTopic() {
+    setIsDeleteModalOpen((prev) => !prev)
   }
+
+  const { register, handleSubmit } = useForm<{ password: string }>({
+    mode: 'onChange',
+  })
+  const commentId = comment.id
+  const onSubmit: SubmitHandler<{ password: string }> = ({ password }) => {
+    deleteMutation.mutate({ topicId, commentId, password })
+  }
+
+  useEffect(() => {
+    if (isDeleteModalOpen) {
+      document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`
+    }
+    return () => {
+      const scrollY = document.body.style.top
+      document.body.style.cssText = ''
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1)
+    }
+  }, [isDeleteModalOpen])
 
   return (
     <>
@@ -58,15 +77,34 @@ export default function Comment({ comment }: Props) {
         </UserName>
         <DateWrapper>
           <span>{formatDate(comment.createdAt, 'absolute')}</span>
-          {isMyComment && <button>삭제</button>}
+          {isMyComment && <button onClick={toggleDeleteTopic}>삭제</button>}
         </DateWrapper>
       </CommentHeader>
       <Content>{comment.content}</Content>
-      {/* <form onSubmit={handleDeleteComment}>
-        <label>
-          비밀번호 <input type="password" ref={passwordInputRef} />
-        </label>
-      </form> */}
+      {isDeleteModalOpen && (
+        <Overlay>
+          <ModalContainer>
+            <p>
+              댓글을 <Delete>삭제</Delete>하시겠습니까?
+            </p>
+            <p>삭제하시려면 비밀번호를 입력해주세요</p>
+            <form id="passwordConfirmForm" onSubmit={handleSubmit(onSubmit)}>
+              <DeleteLabel htmlFor="password">비밀번호</DeleteLabel>
+              <DeleteInput
+                id="password"
+                type="password"
+                {...register('password', {
+                  required: '비밀번호를 입력해주세요.',
+                })}
+              />
+            </form>
+            <div>
+              <CancelButton onClick={toggleDeleteTopic}>취소</CancelButton>
+              <DeleteButton form="passwordConfirmForm">삭제</DeleteButton>
+            </div>
+          </ModalContainer>
+        </Overlay>
+      )}
     </>
   )
 }
@@ -100,4 +138,56 @@ const Content = styled.p`
   color: #7f7f7f;
   line-height: 1.5;
   word-wrap: break-word;
+`
+const Overlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+`
+const ModalContainer = styled.div`
+  width: 300px;
+  border-radius: 14px;
+  color: #7f7f7f;
+  background-color: #ffffff;
+  padding: 20px;
+  text-align: center;
+  line-height: 1.5;
+`
+const DeleteLabel = styled.label`
+  margin-right: 10px;
+`
+const DeleteInput = styled.input`
+  height: 38px;
+  border: none;
+  background-color: #f3f3f3;
+  margin: 10px 0 20px;
+  padding: 0 10px;
+`
+const Delete = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: #38af61;
+`
+const CancelButton = styled.button`
+  width: 124px;
+  height: 37px;
+  font-size: 16px;
+  color: #7f7f7f;
+  border-radius: 50px;
+  background-color: #f3f3f3;
+  margin-right: 10px;
+`
+const DeleteButton = styled.button`
+  width: 124px;
+  height: 37px;
+  font-size: 16px;
+  color: #ffffff;
+  border-radius: 50px;
+  background-color: #38af61;
 `
