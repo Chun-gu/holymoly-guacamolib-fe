96836b4f3713632661e2a4ec2ca2ece547569b45
Chun-gu <chungu.yi92@gmail.com>
2023-05-28 09:19:55 +0900

    메인페이지 스타일링

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,20 @@
 import { useMutation } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { createTopic, topicKeys } from '@/api/topic'
-import { HotTopics, NewTopics } from '@/components'
+import { ReactComponent as Flame } from '@/assets/flame-icon.svg'
+import { ReactComponent as New } from '@/assets/new-icon.svg'
+import { ReactComponent as Pencil } from '@/assets/pencil-icon.svg'
+import { HotTopics, NewTopics, OnboardingModal } from '@/components'
 import { queryClient } from '@/main'
 
 export default function MainPage() {
   const newTopic = {
-    title: '물복 vs 딱복',
-    content: '물복이 좋냐 딱복이 좋냐',
-    firstOption: '물복',
+    title: '물물물물물물물물물물물물물물물물물물물물',
+    content:
+      '물복이 좋냐 딱복이 좋냐 물복복이 좋냐 딱복이 좋냐물복이 좋냐물복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복이 좋냐 딱복이 좋냐물복냐',
+    firstOption: '물물물물물물물물물물물물물물물물물물물물물물물물물물물물',
     secondOption: '딱복',
     password: '123',
   }
@@ -21,18 +26,65 @@ export default function MainPage() {
 
   return (
     <>
+      {/* <OnboardingModal /> */}
       <div>
         <button onClick={() => mutation.mutate(newTopic)}>create todos</button>
       </div>
-      <section>
-        <h2>불타는 주제</h2>
+      <HotTopicSection>
+        <SectionHeading>
+          <Flame />
+          <span>불타는 주제</span>
+        </SectionHeading>
         <HotTopics />
-      </section>
+      </HotTopicSection>
       <section>
-        <h2>새로운 주제</h2>
+        <SectionHeading>
+          <New />
+          <span>새로운 주제</span>
+        </SectionHeading>
         <NewTopics />
       </section>
-      <Link to="newTopic">글쓰기</Link>
+      <WriteNewTopic to="newTopic">
+        <Pencil />
+        글쓰기
+      </WriteNewTopic>
     </>
   )
 }
+
+const HotTopicSection = styled.section`
+  margin-bottom: 49px;
+`
+
+const SectionHeading = styled.h2`
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 10px;
+  height: 37px;
+  span {
+    display: block;
+    font-size: 20px;
+    font-weight: lighter;
+    margin-left: 8px;
+    margin-bottom: 6px;
+  }
+`
+
+const WriteNewTopic = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 115px;
+  font-size: 16px;
+  color: #ffffff;
+  background-color: #41ac62;
+  padding: 8px;
+  border-radius: 50px;
+  position: fixed;
+  right: 50%;
+  transform: translateX(50%);
+  bottom: 30px;
+  svg {
+    margin-right: 4px;
+  }
+`
