8f01aabcac4d4160caa97cda4071fa50ec437beb
Chun-gu <chungu.yi92@gmail.com>
2023-05-28 12:23:21 +0900

    상단 헤더에 로고 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,34 @@
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { ReactComponent as Logo } from '@/assets/logo.svg'
 
 export default function Layout() {
   return (
-    <>
+    <Container>
       <header>
-        <nav>내비게이션 바</nav>
+        <Nav>
+          <Logo />
+        </Nav>
       </header>
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </>
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  width: 375px;
+  min-height: 100vh;
+  margin: 0 auto;
+`
+const Nav = styled.nav`
+  height: 52px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Main = styled.main`
+  padding: 0 20px;
+`
